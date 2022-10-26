9544d4fd9b894b6a264ae0949395401b63d80384
LesterSoto <joellestre@gmail.com>
2022-10-26 08:52:27 -0400

    Toolbar

--- app/src/main/java/com/ramdom/infogramapplication/CreateAccountActivity.java
+++ app/src/main/java/com/ramdom/infogramapplication/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.ramdom.infogramapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getString(R.string.title_create_account),true);
     }
+
+    public void showToolbar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
 }
